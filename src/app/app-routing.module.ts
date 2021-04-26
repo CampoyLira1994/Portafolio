71541ae88dd3fe8c74e0d71c1e237c8f79ce693d
Juan Antonio Campoy Lira <juan.campoy@KOSMOS.local>
2021-04-26 16:11:17 -0500

    Git commit pull

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { ExperienciaComponent } from './experienciaYCursos/experiencia.component';
+import { PortadaComponent } from './portada/portada.component';
+import { PortafolioWebComponent } from './portafolio-web/portafolio-web.component';
+
+
+const routes: Routes = [
+  {path: '',component:PortadaComponent },
+  {path: 'experiencia', component: ExperienciaComponent},
+  {path: 'portafolio', component: PortafolioWebComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ExperienciaComponent } from './experienciaYCursos/experiencia.component';
+import { PortadaComponent } from './portada/portada.component';
+import { ContactoComponent } from './contacto/contacto.component';
+import { PortafolioWebComponent } from './portafolio-web/portafolio-web.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ExperienciaComponent,
+    PortadaComponent,
+    ContactoComponent,
+    PortafolioWebComponent
+
   ],
   imports: [
     BrowserModule,
